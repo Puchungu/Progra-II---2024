d10bf466bdabff0f8886041190eb860dd164d9fd
NELSON OSWALDO ALVARENGA CUADRA <SMSS127921@ugb.edu.sv>
2024-02-12 11:13:00 -0600

    se agrega la rama sensor_proximidad y se hace el desarrollo de como utilizar el sensor

--- controlesBasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ controlesBasicos/app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -2,91 +2,70 @@ package com.ugb.controlesbasicos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     TextView tempVal;
-    Button btn;
-    RadioGroup opt;
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(new View.OnClickListener() {
-
+        tempVal = findViewById(R.id.lblSensorProximidad);
+        activarSensorProximidad();
+    }
+    @Override
+    protected void onResume() {
+        iniciar();
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        detener();
+        super.onPause();
+    }
+    private void activarSensorProximidad(){
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        if(sensor==null){
+            tempVal.setText("Tu telefono NO tiene sensor de proximidad");
+            finish();
+        }
+        sensorEventListener = new SensorEventListener() {
             @Override
-            public void onClick(View view) {
-
-
-
-               /* tempVal = findViewById(R.id.txtnum1);*/
-               /* double num1 = Double.parseDouble(tempVal.getText().toString()); */
-
-                /*tempVal = findViewById(R.id.txtnum2);*/
-                 /*double num2 = Double.parseDouble(tempVal.getText().toString());*/
-
-                EditText editText1 = findViewById(R.id.txtnum1);
-                EditText editText2 = findViewById(R.id.txtnum2);
-                String num1Str = editText1.getText().toString();
-                String num2Str = editText2.getText().toString();
-
-
-               double num1,num2;
-
-                if (num1Str.isEmpty()) {
-                     num1 = 0; // Asignar 0 si está vacío
-                } else {
-                     num1 = Double.parseDouble(num1Str);
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                double valor = sensorEvent.values[0];
+                tempVal.setText("Proximidad: "+ valor);
+                if( valor<=4 ){
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                } else if (valor<=8) {
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                }else{
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
                 }
-
-                if (num2Str.isEmpty()) {
-                     num2 = 0; // Asignar 0 si está vacío
-                } else {
-                     num2 = Double.parseDouble(num2Str);
-                }
-
-                double respuesta = 0;
-                opt = findViewById(R.id.optOpciones);
-                switch (opt.getCheckedRadioButtonId()){
-                    case R.id.optSuma:
-                        respuesta = num1+num2;
-                        break;
-                    case R.id.optResta:
-                        respuesta = num1-num2;
-                        break;
-                    case R.id.optMulplicacion:
-                        respuesta = num1*num2;
-                        break;
-                    case R.id.optDivision:
-                        respuesta = num1/num2;
-                        break;
-                    case R.id.optPorcentaje:
-                        respuesta = (num2/100)*num1;
-                        break;
-                    case R.id.optFactorial:
-                        long factorial = 1;
-                        for (int i = 1; i <= num1; i++) {
-                            factorial *= i;
-                            respuesta = factorial;
-                        }
-                        break;
-                    case R.id.optExponente:
-                        respuesta = Math.pow(num1, num2);
-                        break;
-                    case R.id.optRaiz:
-                        respuesta = Math.pow(num1, 1/num2);
-                        break;
-                }
-                tempVal = findViewById(R.id.lblrespuesta);
-                tempVal.setText("Respuesta: "+ respuesta);
             }
-        });
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+    }
+    private void iniciar(){
+        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
+    }
+    private void detener(){
+        sensorManager.unregisterListener(sensorEventListener);
     }
 }
