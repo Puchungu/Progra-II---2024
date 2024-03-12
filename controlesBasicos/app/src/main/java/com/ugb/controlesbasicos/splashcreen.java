16f3ae01a940363ae272ddded1f20a4354ebbd95
NELSON OSWALDO ALVARENGA CUADRA <SMSS127921@ugb.edu.sv>
2024-03-11 20:37:06 -0600

    se agrego la splash screen

---
+++ controlesBasicos/app/src/main/java/com/ugb/controlesbasicos/splashcreen.java
@@ -0,0 +1,30 @@
+package com.ugb.controlesbasicos;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class splashcreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splashcreen.this, listado_amigos.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea, 4000);
+    }
+}
